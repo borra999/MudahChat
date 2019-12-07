d2e7eb72bf11f3fbcecd3fed2e9b8ceee7efcf07
Borra <bhargav.borra999@gmail.com>
2019-12-07 18:43:50 +0800

    > Created new project

---
+++ MudahChat/MudahChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MudahChat
+//
+//  Created by Bhargav Anjibabu Borra on 27/11/2019.
+//  Copyright © 2019 Mobiversa Sdn. Bhd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
